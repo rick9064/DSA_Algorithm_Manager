d42bb50a6a43a3434f276b69a3e8fda14ce5de61
rick9064 <138608432+rick9064@users.noreply.github.com>
2025-06-30 13:39:39 +0530

    Update firebase.js

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -1,16 +1,1 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBKJD0Dq4EfiHNoxy0M2b3iypw6HLLySko",
-  authDomain: "dsa-algorithm-manager.firebaseapp.com",
-  projectId: "dsa-algorithm-manager",
-  storageBucket: "dsa-algorithm-manager.appspot.com", 
-  messagingSenderId: "469965890938",
-  appId: "1:469965890938:web:e1055628a36bc86fbaa2c3",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
